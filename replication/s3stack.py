a51b5bcccd64595cc65a400fca445df9cbb47227
fossouo <fossouodonald@gmail.com>
2020-10-05 08:22:45 +0200

    Add files via upload

---
+++ replication/s3stack.py
@@ -0,0 +1,122 @@
+from aws_cdk import(
+    core,
+    aws_iam as iam,
+    aws_s3 as s3
+)
+
+class S3Stack(core.Stack):
+
+    def __init__(self,scope:core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope,id,**kwargs)
+
+        self.source_bucket_name_prefix = 'source-s3-bucket-replication-demo-df'
+        self.destination_bucket_name_prefix='destination-s3-bucket-replication-demo-df'
+
+        if(self.region == 'us-east-1'):
+            self.role=iam.Role(
+                self,
+                's3_replica_poc_role',
+                assumed_by=iam.ServicePrincipal('s3.amazonaws.com'),
+                role_name=f's3-replication-role'
+            )
+
+            iam.Policy(
+                self,
+                's3_replica_poc_policy',
+                roles=[
+                    self.role
+                ],
+                    statements=[
+                        self.replication_source_policy(self.get_bucket_arn(self.source_bucket_name_prefix,'us-east-1')),
+                        self.replication_policy(self.get_bucket_arn(self.destination_bucket_name_prefix,'us-west-2'))
+                    ]
+                
+            )
+            
+
+            self.replication_conf = s3.CfnBucket.ReplicationConfigurationProperty(
+                role=self.role.role_arn,
+                
+                rules=[
+                    s3.CfnBucket.ReplicationRuleProperty(
+                        id='rule-replicate-all-data',
+                        destination=s3.CfnBucket.ReplicationDestinationProperty(
+                            encryption_configuration=(),
+                            bucket=self.get_bucket_arn(self.destination_bucket_name_prefix,'us-west-2'),
+                            metrics=s3.CfnBucket.MetricsProperty(
+                                event_threshold=s3.CfnBucket.ReplicationTimeValueProperty(minutes=10),
+                                status='Enabled'
+                            ),
+                        ),
+                    prefix='testing',
+                    status='Enabled'
+#                    delete_marker_replication=s3.CfnBucket.DeleteMarkerReplicationProperty(
+#                        status='Enabled'
+#                    )
+                    )             
+                ]
+            )
+
+            s3.CfnBucket(
+                self,
+                'Bucket-id',
+                access_control='Private',
+                bucket_name=f'{self.source_bucket_name_prefix}-{self.region}',
+                versioning_configuration = s3.CfnBucket.VersioningConfigurationProperty(
+                    status='Enabled'
+                ),
+                replication_configuration=self.replication_conf
+            )
+
+        elif(self.region == 'us-west-2'):
+            s3.CfnBucket(
+                self,
+                'Bucket-id',
+                access_control='Private',
+                bucket_name=f'{self.destination_bucket_name_prefix}-{self.region}',
+                versioning_configuration = s3.CfnBucket.VersioningConfigurationProperty(
+                    status='Enabled'
+                )
+            )
+
+    @staticmethod
+    def replication_source_policy(source_bucket):
+        return iam.PolicyStatement(
+            actions=[
+                's3:Get*',
+                's3:ListBucket',
+            ],
+            effect=iam.Effect.ALLOW,
+            resources=[
+                source_bucket,
+                f'{source_bucket}/*']
+        )
+
+    @staticmethod
+    def get_bucket_arn(bucket_name_prefix,region):
+        return f'arn:aws:s3:::{bucket_name_prefix}-{region}'.lower()
+
+    @staticmethod
+    def replication_policy(destination_bucket_arn):
+        return iam.PolicyStatement(
+            actions=[
+                's3:ReplicateObject',
+                's3:ReplicateDelete',
+                's3:ReplicateTags',
+                's3:GetObjectVersionTagging'
+            ],
+            effect=iam.Effect.ALLOW,
+            resources=[
+                f'{destination_bucket_arn}/*'
+            ]
+        )
+
+        ### Mettre à jour le role iam pour avoir accès à la clé KMS du compte source et compte destination 
+        #### Deux modes : Cross Account / Cross Region 
+        #### NB : les objets sont toujours chiffrés via KMS 
+
+
+        #### Ingestion 
+        ###  1 - Machine locale 
+        ###  2 - via FTP family
+        ###  3 - Ingestion en mode pull (depuis Glue par ex:)
